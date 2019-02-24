fb6238a30ddc0ac3559583c2d3338ac038ef3c7b
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2019-02-24 11:30:33 -0300

    adiciona algoritmo recursivo da busca binaria

--- eda_leda/implementacoes/buscaBinariaRecursiva.py
+++ eda_leda/implementacoes/buscaBinariaRecursiva.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 '''
-Implementação do algoritmo de Busca Binária (Versão Iterativa)
+Implementação do algoritmo de Busca Binária (Versão Recursiva)
 Obs: A busca binária é útil para encontrar, de maneira bastante eficiente, um item em uma lista ordenada de itens.
 
 *** Note que a lista deve ser ordenada, caso contrário o algoritmo não funcionará.
@@ -13,17 +13,16 @@ Obs: A busca binária é útil para encontrar, de maneira bastante eficiente, um
     https://pt.khanacademy.org/computing/computer-science/algorithms/binary-search/a/implementing-binary-search-of-an-array
     https://pt.khanacademy.org/computing/computer-science/algorithms/binary-search/a/running-time-of-binary-search
 '''
+	
+def buscaBinariaRecursiva(alvo, array):
+    return buscaBinariaRecursivaAux(alvo, array, 0, len(array) - 1)
 
-def buscaBinariaIterativa(alvo, array):
-	min = 0
-	max = len(array) - 1
-	while min <= max:
-		mid = (min + max)/2
-		if array[mid] == alvo:
-			return array[mid]
-		else :
-			if array[mid] < alvo: # Eliminamos os numeros a esquerda de mid da nossa busca
-				min = mid + 1
-			else: 				  # Eliminamos os números a direita de mid da nossa busca
-				max = mid - 1
-
+def buscaBinariaRecursivaAux(alvo, array, min, max):
+    mid = (min + max)/2
+    if array[mid] == alvo:
+        return array[mid]
+    else :
+        if array[mid] < alvo: # Eliminamos os numeros a esquerda de mid da nossa busca
+            return buscaBinariaRecursivaAux(alvo, array, mid + 1, max)
+        else: 				  # Eliminamos os números a direita de mid da nossa busca
+            return buscaBinariaRecursivaAux(alvo, array, min, mid - 1)
