3f34dc6b044a85e242705c766563b9379dbba6d7
Ionesio Junior <ionesio@lsd.ufcg.edu.br>
2019-04-03 11:59:36 -0300

    Tradução/Adaptação ao estilo de documentação exigido pelo padrão PEP8

--- eda_leda/implementacoes/sorting_algorithms/GnomeSort.py
+++ eda_leda/implementacoes/sorting_algorithms/GnomeSort.py
@@ -5,27 +5,44 @@ from Sort import Sort
 __author__ = "Ionesio Junior"
 
 class GnomeSort(Sort):
-	
-	''' Gnome sort class have many ways to implement gnome sort algorithm idea '''
-	
+	"""Implementação do algoritmo de Ordenação GnomeSort
+        
+        Classe para abstração do algoritmo de Ordenação GnomeSort implementado de forma interativa e recursiva
+        
+        Complexidade do Algoritmo: O(n^2)
+        """
 
 	def sort(self,array ,leftIndex = 0,rightIndex = None):
-        	'''Generic Sort
-                algorithm.sort(array) -> return complete ordered array
-                algorithm.sort(array,leftLimit,rightLimit) -> return partially ordered array
-        	'''  
+        	"""Método genérico de interface para aplicar 
+                   o algoritmo de ordenação no array por completo ou usando limites
+                
+                Parametros:
+                    array: Lista a ser ordenada
+                    leftIndex(Opcional): Índice limitrofe a esquerda do array para ordenação parcial
+                    rightIndex(Opcional): Índice limitrofe a direita do array para ordenação parcial
+
+                Exemplo de execução:
+                    algorithm.sort(array): Retorna a lista completamente ordenada
+                    algorithm.sort(array,leftLimit,rightLimit): Retorna a lista parcialmente ordenada
+        	"""  
 		if(rightIndex == None):
 			rightIndex = len(array) - 1
 		if self.__validateParams(array,leftIndex,rightIndex):
-			#Choose the type of GnomeSort
-                        self.__simpleGnomeSort(array,leftIndex,rightIndex)		
+			# Escolher a abordagem a ser utilizada (Interativa / Recursiva)
+                        self.__interactiveGnomeSort(array,leftIndex,rightIndex)		
 			#self.__recursiveGnomeSort(array,leftIndex,leftIndex + 1 ,rightIndex)
 	
 	
 	
-	def __simpleGnomeSort(self,array,leftIndex,rightIndex):
-		''' Simple iteractive Gnome sort implementation '''
-		pivot = leftIndex +1
+	def __interactiveGnomeSort(self,array,leftIndex,rightIndex):
+		"""Implementação do método GnomeSort de forma interativa
+		   
+                Parametros:
+                    array: Lista a ser ordenada
+                    leftIndex(Opcional): Índice limítrofe a esquerda do array para ordenação parcial
+                    rightIndex(Opcional): Índice limítrofe a direita do array para ordenação parcial
+                """
+                pivot = leftIndex +1
 		while(pivot <= rightIndex):
 			if(array[pivot] >= array[pivot -1]):
 				pivot = pivot +1
@@ -40,8 +57,14 @@ class GnomeSort(Sort):
 	
 	
 	def __recursiveGnomeSort(self,array,leftIndex,pivot,rightIndex):
-		''' Recursive Gnome Sort implementation '''
-		if(pivot <= rightIndex):
+		"""Implementação do método GnomeSort de forma recursiva
+		
+                Parametros:
+                    array: Lista a ser ordenada
+                    leftIndex(Opcional): Índice limítrofe a esquerda do array para ordenação parcial
+                    rightIndex(Opcional): Índice limítrofe a direita do array para ordenação parcial
+                """
+                if(pivot <= rightIndex):
 			if(array[pivot] >= array[pivot - 1]):
 				self.__recursiveGnomeSort(array,leftIndex,pivot + 1 , rightIndex)
 			else:
@@ -53,8 +76,20 @@ class GnomeSort(Sort):
 
 	
 	def __validateParams(self,array,leftIndex,rightIndex):
-	        ''' This method verify if parameters are valid if some parameter is not valid the method returns false '''
-		if(array == None or leftIndex < 0 or rightIndex > len(array) - 1 or leftIndex > rightIndex):
+	        """Verificação da validade dos parametros de limite passados
+		
+                Este método busca evitar entradas inválidas para o algoritmo de ordenação, tais como: índices negativos,
+                índice direito menor que o esquerdo, índice direito superior ao tamanho do array.
+
+                Parametros:
+                    array: Lista a ser ordenada
+                    leftIndex(Opcional): Índice limítrofe a esquerda do array para ordenação parcial
+                    rightIndex(Opcional): Índice limítrofe a direita do array para ordenação parcial
+
+                Retorna:
+                    Boolean: Indicando se os parâmetros são ou não válidos
+                """
+                if(array == None or leftIndex < 0 or rightIndex > len(array) - 1 or leftIndex > rightIndex):
 			return False
 		else:
 			return True
