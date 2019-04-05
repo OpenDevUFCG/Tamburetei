221d84682767965d41564447950d917ca774d0aa
Thayanne Luiza <thayannevls@gmail.com>
2019-04-04 23:46:40 -0300

    Update eda_leda/implementacoes/sorting_algorithms/SelectionSort.py
    Co-Authored-By: IonesioJunior <ionesiojr@gmail.com>

--- eda_leda/implementacoes/sorting_algorithms/SelectionSort.py
+++ eda_leda/implementacoes/sorting_algorithms/SelectionSort.py
@@ -44,7 +44,7 @@ class SelectionSort(Sort):
 		while(leftIndex < rightIndex):
 			minIndex = leftIndex
 			for i in range(leftIndex + 1, rightIndex + 1):
-				if(array[minIndex]> array[i]):
+				if(array[minIndex] > array[i]):
 					minIndex = i
 			array[minIndex],array[leftIndex] = array[leftIndex] ,array[minIndex]
 			leftIndex = leftIndex + 1
