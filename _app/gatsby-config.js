8cea46aa5996345fc8a6119f18d9947938a757be
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-10-04 18:23:21 -0300

    ignora arquivos dentro de dotfiles

--- _app/gatsby-config.js
+++ _app/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       options: {
         path: `${__dirname}/..`,
         name: 'subjects',
-        ignore: ['**/\\.*', '**/_app/**/*', '**/scripts'],
+        ignore: ['**/\\.*', '**/\\.*/**/*', '**/_app/**/*', '**/scripts/**/*'],
       },
     },
     {
