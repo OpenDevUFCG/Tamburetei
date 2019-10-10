6646a472a030cca72fdbe809e71196af6b2c0906
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-10-10 20:11:24 -0300

    adiciona footer com logo do opendev

--- _app/src/components/Layout.js
+++ _app/src/components/Layout.js
@@ -10,6 +10,7 @@ import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from './Header'
+import OpenDevLogo from './OpenDevLogo'
 import styles from './Layout.module.css'
 
 const Layout = ({ children }) => {
@@ -24,16 +25,31 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <div className={styles.container}>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div className={styles.container}>
-        <main>{children}</main>
-        <footer>
-          &copy; {new Date().getFullYear()}, Built with{' '}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+      <main>{children}</main>
+      <footer className={styles.footer}>
+        <div className={styles.footerContainer}>
+          <a
+            href="https://opendevufcg.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <OpenDevLogo size={100} />
+          </a>
+          <p className={styles.copyRight}>
+            &copy; {new Date().getFullYear()} OpenDevUFCG, feito com{' '}
+            <a
+              href="https://www.gatsbyjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Gatsby
+            </a>
+          </p>
+        </div>
+      </footer>
+    </div>
   )
 }
 
