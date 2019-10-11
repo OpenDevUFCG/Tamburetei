150c3de16d36ce95b3577da9a46b43fd922c51bb
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-10-11 09:56:07 -0300

    atualiza layout da página de cadeiras

--- _app/src/pages/cadeiras.js
+++ _app/src/pages/cadeiras.js
@@ -1,16 +1,59 @@
+import classNames from 'classnames'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import SubjectLayout from '../components/SubjectLayout'
+import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
-const CadeirasPage = ({ location }) => {
+import styles from './cadeiras.module.css'
+
+const SUBJECTS_QUERY = graphql`
+  {
+    subjects: allMarkdownRemark(
+      filter: {
+        fileAbsolutePath: { regex: "/README\\.md/" }
+        fields: { slug: { regex: "/^\\/[\\w]+\\/$/" } }
+      }
+      sort: { fields: [frontmatter___title], order: ASC }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+          }
+          fields {
+            slugPath
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+const CadeirasPage = () => {
+  const data = useStaticQuery(SUBJECTS_QUERY)
+
   return (
-    <SubjectLayout location={location}>
+    <Layout className={styles.container}>
       <SEO />
-      <h2>Cadeiras</h2>
-      <p>Aqui você pode encontrar nosso layout básico para todas as páginas!</p>
-    </SubjectLayout>
+      <h2 className={styles.title}>Disciplinas</h2>
+      <section className={styles.section}>
+        {data.subjects.edges.map(subject => (
+          <div key={subject.node.id} className={styles.cadeiraWrapper}>
+            <Link
+              to={subject.node.fields.slugPath}
+              className={classNames('link', styles.cadeirasLink)}
+            >
+              {subject.node.frontmatter.title}
+            </Link>
+            <p>{subject.node.excerpt}</p>
+          </div>
+        ))}
+      </section>
+    </Layout>
   )
 }
 
