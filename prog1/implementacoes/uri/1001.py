d08edf17e1ea38903bc8c492bb421bf170f358bc
Maria Eduarda de Azevedo <maria-eduarda-azevedo@hotmail.com>
2019-10-19 17:49:54 -0300

    Adicionada resolução da questao 1001 em Python 3

--- prog1/implementacoes/uri/1001.py
+++ prog1/implementacoes/uri/1001.py
@@ -1,5 +1,11 @@
+#Questao 1001 do URI
+
+#Variavel que recebe um inteiro da entrada
 A = int(input())
+#Variavel que recebe um inteiro da entrada
 B = int(input())
+#Somando as variaveis A e B
 X = A + B
+#Jogando X na saida formatada
 print('X = {}'.format(X))
 
