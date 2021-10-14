65c3003ab32e53a5eed74fa17e31dc2935f7f9d3
Guilherme Aureliano <49173189+GuilhermeAureliano@users.noreply.github.com>
2021-10-14 17:13:11 -0300

    Update haskell.hs

--- plp/implementacoes/haskell.hs
+++ plp/implementacoes/haskell.hs
@@ -1,4 +1,7 @@
 --- FUNÇÃO SPLIT QUE GERA UMA LISTA DE STRING ---
+--- wordsWhen(==' ') "A OpenDev eh muito massa" RESUTLADO -> ["A","OpenDev","eh","muito","massa"] ---
+--- wordsWhen(==',') "1, 2, 3, 4, 5" RESULTADO -> ["1"," 2"," 3"," 4"," 5"] ---
+--- FAZ O SPLIT DE ACORDO COM O PARÂMETRO ---
 wordsWhen     :: (Char -> Bool) -> String -> [String]
 wordsWhen p s =  case dropWhile p s of
                       "" -> []
@@ -6,6 +9,8 @@ wordsWhen p s =  case dropWhile p s of
                             where (w, s'') = break p s'
 
 --- GERA UMA LISTA DE LISTA SEM A LISTA QUE CONTEM O STRING PASSADO COMO PARÂMETRO ---
+--- geraLista "111" [["111", "Guilherme"], ["222", "Fulano"]] ---
+--- RESULTADO -> [["222","Fulano"], ["333","Sicrano"]] ---
 geraLista :: String -> [[String]] -> [[String]]
 geraLista _ [] = []
 geraLista v (x:xs) | (aux v x) == True = geraLista v xs
